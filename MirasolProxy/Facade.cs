31722d293fa4bef376b095f7de2645c357442cc1
unknown <klausgaarde@live.dk>
2015-12-01 00:40:47 +0100

    Startup

---
+++ MirasolProxy/Facade.cs
@@ -0,0 +1,17 @@
+﻿using MirasolProxy.Proxies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirasolProxy
+{
+    public class Facade
+    {
+        public ApartmentProxyService GetApartmentProxy()
+        {
+            return new ApartmentProxyService;
+        }
+    }
+}
---
+++ MirasolProxy/Proxies/ApartmentProxyService.cs
@@ -0,0 +1,22 @@
+﻿using DTOModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirasolProxy.Proxies
+{
+    public class ApartmentProxyService
+    {
+        public IEnumerable<Apartment> ReadAll()
+        {
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync("http://localhost:41434/api/apartment/").Result;
+                return response.Content.ReadAsAsync<IEnumerable<Apartment>>().Result;
+            }
+        }
+    }
+}
