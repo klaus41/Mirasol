ed05f91fca1500a618ddb048d8276ad0d7148971
unknown <klausgaarde@live.dk>
2015-12-03 15:29:21 +0100

    adding facilities and expanding apartments

--- DTOModel/Apartment.cs
+++ DTOModel/Apartment.cs
@@ -11,6 +11,21 @@ namespace DTOModel
     {
         public int Id { get; set; }
         public string Address { get; set; }
-        public int HouseNumber { get; set; }
+        public int Bedrooms { get; set; }
+        public int Sleeps { get; set; }
+        public int Bathrooms { get; set; }
+        public int Toilets { get; set; }
+        public int LivingArea { get; set; }
+        public int Terrace { get; set; }
+        public string Pool { get; set; }
+        public string Jacuzzi { get; set; }
+        public bool WiFi { get; set; }
+        public bool RoofTerrace { get; set; }
+        public bool Garden { get; set; }
+        public bool TerraceFurniture { get; set; }
+        public bool Satellite { get; set; }
+        public bool Balcony { get; set; }
+        public bool AirCondition { get; set; }
+        public bool Heating { get; set; }
     }
 }
